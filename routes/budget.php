015730169ae2242946ddb8886b9e6a5a3df49824
Joshua Anderton <info@joshanderton.com>
2022-03-16 20:46:57 -0700

    Cleanup

--- routes/budget.php
+++ routes/budget.php
@@ -3,7 +3,7 @@
 use TallSaas\Budget\Http\Livewire\Dashboard;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['web', 'auth'])->group(function () {
+Route::middleware(['web', 'auth'])->group(function() {
 
   Route::prefix('budget')->group(function() {
     Route::get('dashboard', Dashboard::class)->name('tallsaas.budget.dashboard');
